cea6264b480ced1620a719bd639b9bd6c82a1ddf
gtorresv <giselle.torres15@gmail.com>
2023-07-17 16:03:46 -0400

    added event listener to burger icon on both html pages

--- assets/js/script.js
+++ assets/js/script.js
@@ -250,4 +250,10 @@ var generateFakeCard = function() {
 
 generateFakeCard();
 
+// mobile navbar menu
+var burgerIcon = document.querySelector('#burger');
+var navbarMenu = document.querySelector('#nav-links');
 
+burgerIcon.addEventListener('click', () => {
+    navbarMenu.classList.toggle('is-active');
+});
